8d95b8ec9657b3eba09233e0e1b16a3e20a887ec
Dave Gomez <davegomez426@gmail.com>
2023-11-25 18:53:24 -0300

    Correccion de repo

--- src/main/java/com/mycompany/webbankingg13/WebBankingG13.java
+++ src/main/java/com/mycompany/webbankingg13/WebBankingG13.java
@@ -1,4 +1,4 @@
-package main;
+package com.mycompany.webbankingg13;
 
 import Interfaz.InterfazEstatica;
 public class Lp2FinalMain {
@@ -7,8 +7,6 @@ public class Lp2FinalMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Prueba");
         InterfazEstatica p1 = new InterfazEstatica();
         p1.setVisible(true);
         
