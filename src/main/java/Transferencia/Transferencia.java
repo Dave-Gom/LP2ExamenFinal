7733a9bbce5fc4fc76567f9da9c07dbb1014762e
Jaime Núñez Viedma <jaimellave12@fpuna.edu.py>
2023-11-26 13:48:39 -0300

    Transferencia sin trannsferir

---
+++ src/main/java/Transferencia/Transferencia.java
@@ -0,0 +1,51 @@
+
+package Transferencia;
+
+/**
+ * Clase Transferencia
+ * 
+ * @author Jaime Núñez Viedma
+ */
+public class Transferencia {
+    private String cuentaOrigen;
+    private String entidadDestino;
+    private String monto;
+    private String bancoOrigen;
+    private String bancoDestino;
+    private String transferenciasCol;
+
+    public Transferencia(String cuentaOrigen, String entidadDestino, String monto, String bancoOrigen,
+            String bancoDestino, String transferenciasCol) {
+        this.cuentaOrigen = cuentaOrigen;
+        this.entidadDestino = entidadDestino;
+        this.monto = monto;
+        this.bancoOrigen = bancoOrigen;
+        this.bancoDestino = bancoDestino;
+        this.transferenciasCol = transferenciasCol;
+    }
+
+    public String getCuentaOrigen() {
+        return cuentaOrigen;
+    }
+
+    public String getEntidadDestino() {
+        return entidadDestino;
+    }
+
+    public String getMonto() {
+        return monto;
+    }
+
+    public String getBancoOrigen() {
+        return bancoOrigen;
+    }
+
+    public String getBancoDestino() {
+        return bancoDestino;
+    }
+
+    public String getTransferenciasCol() {
+        return transferenciasCol;
+    }
+
+}
