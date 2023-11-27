509ecbb74c4a63d100465b8092f1f5d077a77fee
Dave Gomez <davegomez426@gmail.com>
2023-11-27 18:53:06 -0300

    servicios

--- src/webbankingg13/WebBankingG13.java
+++ src/webbankingg13/WebBankingG13.java
@@ -1,9 +1,9 @@
 package webbankingg13;
 
-
 import BaseDeDatos.BaseDeDatos;
 import BaseDeDatos.Seeders;
 import Interfaz.InterfazLogin1;
+import Servicio.Servicio;
 
 /**
  *
@@ -14,7 +14,6 @@ public class WebBankingG13 {
     public static void main(String[] args) {
         BaseDeDatos mainDB = new BaseDeDatos(); // crea una base de datos
         Seeders.seed(mainDB); // carga los valore iniciales en la base de datos
-        
         InterfazLogin1 l1 = new InterfazLogin1(mainDB);
         l1.setVisible(true);
     }
